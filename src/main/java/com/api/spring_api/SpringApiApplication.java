3b3954ae61a2b2a5d230713013bbffe7396219d5
shkim <shkim0922@naver.com>
2024-01-11 18:38:17 +0900

    포트수정

--- src/main/java/com/api/spring_api/SpringApiApplication.java
+++ src/main/java/com/api/spring_api/SpringApiApplication.java
@@ -18,7 +18,7 @@ public class SpringApiApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:14401");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
                 registry.addMapping("/**").allowedOrigins("http://192.168.219.108:14401");
             }
         };
